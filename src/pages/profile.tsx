d9a955f81b7e8b87729d714881da793405b2f8f8
SparshKumarZ <sparsh.kumar@zomato.com>
2025-08-27 00:57:59 +0530

    build fixed

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -4,10 +4,27 @@ import { PROFILE_API } from "@/constants/api";
 import { getWithExpiry } from "@/utils/storage";
 import React, { useEffect, useState } from "react";
 
+interface ProfileData {
+    user_name: string;
+    email: string;
+    name: string;
+    phone_number: string;
+    cash: number;
+    user_type: string;
+}
+
+interface CardData {
+    number: string;
+    rarity: string;
+    name: string;
+    description: string;
+    image: string;
+}
+
 export default function ProfilePage() {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState<ProfileData | null>(null);
     const [loading, setLoading] = useState(true);
-    const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState<CardData[]>([]);
 
     const name = "Sparsh Kumar";
     const avatarUrl = "/avatar.png"; // Make sure this file exists in /public
